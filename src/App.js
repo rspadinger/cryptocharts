f41c2a85f6a9ad462846bbd6fafd0645b1214e7a
Lucas <lsalvatore2@gmail.com>
2018-08-16 11:15:51 -0400

    Add Styled Components, Remove unused styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const CustomElement = styled.div`
+  color: green;
+  font-size: 30px;
+`
+
+const BlueElement = CustomElement.extend`
+  color: blue;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to CryptoDash</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <CustomElement>
+        Hello
+      </CustomElement>
+      <BlueElement>
+        Hello Blue
+      </BlueElement>
       </div>
     );
   }
